d820d00b95a06f246b3eba9717b5521773ac2945
Zeel2103 <yagnik.zeel@gmail.com>
2025-03-11 11:31:42 +0000

    Baggage database created.

--- GroupCoursework/Controllers/BaggageController.cs
+++ GroupCoursework/Controllers/BaggageController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GroupCoursework.Data;
+using GroupCoursework.Models.Entities;
+using GroupCoursework.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GroupCoursework.Controllers
@@ -7,6 +10,62 @@ namespace GroupCoursework.Controllers
     [ApiController]
     public class BaggageController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public BaggageController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //Get all Baggage.
+        [HttpGet]
+        public IActionResult GetAllBaggage()
+        {
+            var allBaggage = dbContext.Baggage.ToList();
+
+            return Ok(allBaggage);
+        }
+
+        //Search Baggage by their baggage Id.
+        [HttpGet]
+        [Route("{BaggageId:int}")]
+        public IActionResult GetBaggageByBaggageId(int BaggageId)
+        {
+            var baggage = dbContext.Passengers.Find(BaggageId);
+
+            if (baggage == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(baggage);
+        }
+
+        //Add baggage to database
+        [HttpPost]
+        public IActionResult AddBaggage(AddBaggageDto addBaggageDto)
+        {
+            var passenger = dbContext.Passengers.FirstOrDefault(p => p.PassportID == addBaggageDto.PassportId);
+            if (passenger == null)
+            {
+                return BadRequest("Passenger with given PassportID does not exist.");
+            }
+
+
+            var baggageEntity = new Baggage()
+            {
+                BaggageID = addBaggageDto.BaggageID,
+
+                PassportId = addBaggageDto.PassportId,
+                
+            };
+
+            dbContext.Baggage.Add(baggageEntity);
+            dbContext.SaveChanges();
+
+            return Ok(baggageEntity);
+        }
+
 
     }
 }

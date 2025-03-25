fb9dc1e35c9d2f2ac86b128d2127c15d171726d8
Zeel2103 <yagnik.zeel@gmail.com>
2025-03-25 11:06:32 +0000

    Employee database created.

---
+++ GroupCoursework/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using GroupCoursework.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupCoursework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //Get all Employees.
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+
+    }
+}

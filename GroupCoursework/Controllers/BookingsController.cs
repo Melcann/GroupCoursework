31f9fd51d1ce8c93640600b9c91fc817cf08a8f8
Zeel2103 <yagnik.zeel@gmail.com>
2025-04-03 11:56:43 +0100

    Bookings search and add new bookings

--- GroupCoursework/Controllers/BookingsController.cs
+++ GroupCoursework/Controllers/BookingsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GroupCoursework.Data;
+using GroupCoursework.Models.Entities;
+using GroupCoursework.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GroupCoursework.Controllers
@@ -7,5 +10,65 @@ namespace GroupCoursework.Controllers
     [ApiController]
     public class BookingsController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public BookingsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //Get all Bookings.
+        [HttpGet]
+        public IActionResult GetAllBookings()
+        {
+            var allbookings = dbContext.Bookings.ToList();
+
+            return Ok(allbookings);
+        }
+
+        //Search Bookings by their booking Id.
+        [HttpGet]
+        [Route("{BookingId}")]
+        public IActionResult GetBookingsByBookingsId(string BookingId)
+        {
+            var bookings = dbContext.Bookings.Find(BookingId);
+
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bookings);
+        }
+
+        //Add bookings to database
+        [HttpPost]
+        public IActionResult AddBookings(AddBookingsDto addBookingsDto)
+        {
+
+            var bookingsEntity = new Bookings()
+            {
+                PassportId = addBookingsDto.PassportId,
+
+                FlightID = addBookingsDto.FlightID,
+
+                PaymentStatus = addBookingsDto.PaymentStatus,
+
+                SeatNumber = addBookingsDto.SeatNumber,
+
+            };
+
+            dbContext.Bookings.Add(bookingsEntity);
+            dbContext.SaveChanges();
+
+            return Ok(bookingsEntity);
+        }
+
+
+
+
+
+
+
     }
 }
---
+++ GroupCoursework/Models/AddBookingsDto.cs
@@ -0,0 +1,14 @@
+﻿using GroupCoursework.Models.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GroupCoursework.Models
+{
+    public class AddBookingsDto
+    {
+        public required int PassportId { get; set; }
+        public required int FlightID { get; set; }
+        public Boolean PaymentStatus { get; set; } = true;
+        public required int SeatNumber { get; set; }
+
+    }
+}

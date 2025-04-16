c913fb9c711df129c37b030e2f3cd13b738db917
Zeel2103 <yagnik.zeel@gmail.com>
2025-04-16 18:30:12 +0100

    Added Admin database and Create and Read operations.

---
+++ GroupCoursework/Models/AddAdminDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GroupCoursework.Models
+{
+    public class AddAdminDto
+    {
+        public required string UserName { get; set; }
+        public required int BranchId { get; set; }
+        public required string Password { get; set; }
+
+    }
+}

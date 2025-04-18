f1ea11100026c3df0bddfa3095fec86bd9f56480
Zeel2103 <yagnik.zeel@gmail.com>
2025-04-18 10:06:46 +0100

    Added loginDto file

---
+++ GroupCoursework/Models/AddLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GroupCoursework.Models
+{
+    public class AddLoginDto
+    {
+        public int BranchId { get; set; }
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+}
